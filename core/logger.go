bf0264a75568a90bd1dcfe0d7c96d3a6db309641
Alex White <VVu@geekfarm.org>
2025-10-09 12:38:44 -0700

    feat: add support for Ds18b20 temp sensor

--- core/logger.go
+++ core/logger.go
@@ -1,7 +1,8 @@
 package core
 
 type Logger interface {
-	Info(msg string, args ...interface{})
-	Error(msg string, args ...interface{})
 	Debug(msg string, args ...interface{})
+	Info(msg string, args ...interface{})
+	Warn(msg string, args ...interface{})
+	Error(msg string, args ...interface{})
 }
---
+++ x/temp/temp.go
@@ -0,0 +1,81 @@
+package temp
+
+import (
+	"errors"
+	"fmt"
+	"keyop/core"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var devicePath string
+
+func NewTempCmd(deps core.Dependencies) *cobra.Command {
+	tmpCmd := &cobra.Command{
+		Use:   "temp",
+		Short: "Temp Utility",
+		Long:  `Read a Ds18b20 temperature sensor and display the message data.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			_, err := temp(deps)
+			return err
+		},
+	}
+
+	tmpCmd.Flags().StringVarP(&devicePath, "device", "d", "/sys/bus/w1/devices/28-000006388d49/w1_slave", "Device Path")
+
+	return tmpCmd
+}
+
+type Temp struct {
+	Now   time.Time `json:"now"`
+	TempC float32   `json:"temp_c,omitempty"`
+	TempF float32   `json:"temp_f,omitempty"`
+	Error string    `json:"error,omitempty"`
+}
+
+func temp(deps core.Dependencies) (Temp, error) {
+	deps.Logger.Debug("temp called")
+
+	temp := Temp{
+		Now: time.Now(),
+	}
+
+	contentBytes, err := os.ReadFile(devicePath)
+	if err != nil {
+		temp.Error = fmt.Sprintf("could not read from %s: %s", devicePath, err.Error())
+		deps.Logger.Info("temp", "data", temp)
+		return temp, err
+	}
+
+	content := string(contentBytes)
+
+	if len(content) == 0 {
+		temp.Error = fmt.Sprintf("no content retrieved from temp device %s", devicePath)
+		deps.Logger.Error("temp", "data", temp)
+		return temp, errors.New(temp.Error)
+	}
+
+	idx := strings.Index(content, "t=")
+
+	tempString := content[idx+2 : len(content)-1]
+
+	deps.Logger.Debug("Ds18b20", "RAW TEMP", tempString)
+
+	tempInt, err := strconv.Atoi(tempString)
+	if err != nil {
+		temp.Error = fmt.Sprintf("unable to convert temp string to int: %d: %s", tempInt, err.Error())
+		deps.Logger.Error("temp", "data", temp)
+		return temp, errors.New(temp.Error)
+	}
+
+	temp.TempC = float32(tempInt) / 1000
+	temp.TempF = float32(temp.TempC*9/5 + 32.0)
+
+	deps.Logger.Info("temp", "data", temp)
+
+	return temp, nil
+}
