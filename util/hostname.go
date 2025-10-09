b7d45fd1ef441858d32cab1e4e09ab41fa498127
Alex White <VVu@geekfarm.org>
2025-10-08 23:30:32 -0700

    feat: add hostname to heartbeat message

---
+++ util/hostname.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"os"
+	"strings"
+)
+
+// GetShortHostname returns the system hostname without the domain suffix.
+// For example, "host.example.com" becomes "host".
+// It returns an error if the hostname cannot be determined.
+func GetShortHostname() (string, error) {
+	shortHostname, err := os.Hostname()
+	if err != nil {
+		return "", err
+	}
+	if idx := strings.Index(shortHostname, "."); idx != -1 {
+		shortHostname = shortHostname[:idx]
+	}
+	return shortHostname, nil
+}
