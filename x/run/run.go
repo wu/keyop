d203e0b383159104236a253873c3aa559b10115b
Alex White <VVu@geekfarm.org>
2025-10-11 14:49:51 -0700

    refactor: move run cobra command

--- x/run/run.go
+++ x/run/run.go
@@ -1,42 +1,10 @@
 package run
 
 import (
-	"fmt"
 	"keyop/core"
 	"sync"
 	"time"
-
-	"github.com/spf13/cobra"
 )
-
-func NewCmd(deps core.Dependencies) *cobra.Command {
-	runCmd := &cobra.Command{
-		Use:   "run",
-		Short: "Run Utility",
-		Long: `Spawn a process to run the extensions at scheduled intervals.
-
-This utility is a work in progress.
-
-`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			logger := deps.MustGetLogger()
-
-			// load the service configuration before calling the run method
-			svcs, err := loadServices(deps)
-			if err != nil {
-				logger.Error("config load", "error", err)
-				return err
-			}
-			if len(svcs) == 0 {
-				logger.Error("config load", "error", "no services configured")
-				return fmt.Errorf("no services configured")
-			}
-			return run(deps, svcs)
-		},
-	}
-
-	return runCmd
-}
 
 func run(deps core.Dependencies, serviceConfigs []core.ServiceConfig) error {
 	logger := deps.MustGetLogger()
@@ -57,6 +25,7 @@ func run(deps core.Dependencies, serviceConfigs []core.ServiceConfig) error {
 			// execute first check immediately
 			if err := service.Check(); err != nil {
 				logger.Error("check", "error", err)
+				// TODO: send to errors channel
 			}
 
 			// start a ticker to execute the check at the specified frequency
@@ -68,6 +37,7 @@ func run(deps core.Dependencies, serviceConfigs []core.ServiceConfig) error {
 				case <-ticker.C:
 					if err := service.Check(); err != nil {
 						logger.Error("check", "error", err)
+						// TODO: send to errors channel
 					}
 				case <-ctx.Done():
 					return
