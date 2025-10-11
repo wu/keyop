a6d68affb925b0484de64bb14cf2cb48fb5f2e67
Alex White <VVu@geekfarm.org>
2025-10-11 14:33:50 -0700

    test: adding tests for deps

---
+++ core/dependencies_test.go
@@ -0,0 +1,57 @@
+package core
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDependencies_MustGetLogger_PanicsWhenUnset(t *testing.T) {
+	var d Dependencies
+	assert.Panics(t, func() { _ = d.MustGetLogger() })
+}
+
+func TestDependencies_MustGetLogger_ReturnsWhenSet(t *testing.T) {
+	var d Dependencies
+	var buf bytes.Buffer
+	logger := slog.New(slog.NewJSONHandler(&buf, nil))
+
+	d.SetLogger(logger)
+
+	got := d.MustGetLogger()
+	assert.Equal(t, logger, got)
+
+	// use the logger to ensure it behaves and does not panic
+	got.Info("test", "key", "value")
+}
+
+func TestDependencies_MustGetContext_PanicsWhenUnset(t *testing.T) {
+	var d Dependencies
+	assert.Panics(t, func() { _ = d.MustGetContext() })
+}
+
+func TestDependencies_MustGetContext_ReturnsWhenSet(t *testing.T) {
+	var d Dependencies
+	ctx := context.WithValue(context.Background(), "k", "v")
+	d.SetContext(ctx)
+
+	got := d.MustGetContext()
+	assert.Equal(t, ctx, got)
+}
+
+func TestDependencies_MustGetOs_PanicsWhenUnset(t *testing.T) {
+	var d Dependencies
+	assert.Panics(t, func() { _ = d.MustGetOsProvider() })
+}
+
+func TestDependencies_MustGetOs_ReturnsWhenSet(t *testing.T) {
+	var d Dependencies
+	osProvider := OsProvider{}
+	d.SetOsProvider(osProvider)
+
+	got := d.MustGetOsProvider()
+	assert.Equal(t, osProvider, got)
+}
