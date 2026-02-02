e9203cb918cb350daafa78b140b601d43a186720
Alex White <VVu@geekfarm.org>
2026-02-01 22:49:18 -0800

    ci: add simple build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o nounset -o errexit -o pipefail -o errtrace
+
+echo "Building keyop for macos"
+go build -o output/keyop
+
+echo "Building keyop for linux amd64"
+env GOOS=linux GOARCH=amd64 go build -o output/keyop-linux-amd64
+
+echo "Building keyop for linux arm64"
+env GOOS=linux GOARCH=arm64 go build -o output/keyop-linux-arm64
+
