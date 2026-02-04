6eb247c2da524758f6605e7abf43a3f680397433
Alex White <VVu@geekfarm.org>
2026-02-03 18:56:21 -0800

    feat: add graphite plugin
    - update heartbeat to send uptime to metrics channel

---
+++ x/graphite/service.go
@@ -0,0 +1,114 @@
+package graphite
+
+import (
+	"fmt"
+	"keyop/core"
+	"keyop/util"
+	"time"
+
+	"github.com/marpaia/graphite-golang"
+)
+
+var startTime time.Time
+
+func init() {
+	// capture the service start time for reporting uptime
+	startTime = time.Now()
+}
+
+type Service struct {
+	Deps     core.Dependencies
+	Cfg      core.ServiceConfig
+	Graphite *graphite.Graphite
+	Host     string
+	Port     int
+}
+
+func NewService(deps core.Dependencies, cfg core.ServiceConfig) core.Service {
+	return &Service{
+		Deps: deps,
+		Cfg:  cfg,
+	}
+}
+
+func (svc *Service) ValidateConfig() []error {
+	logger := svc.Deps.MustGetLogger()
+
+	var errs []error
+
+	subErrs := util.ValidateConfig("subs", svc.Cfg.Subs, []string{"graphite"}, logger)
+	if len(subErrs) > 0 {
+		errs = append(errs, subErrs...)
+	}
+
+	// check port
+	port, portExists := svc.Cfg.Config["port"].(int)
+	if !portExists {
+		err := fmt.Errorf("graphite: port not set in config")
+		logger.Error(err.Error())
+		errs = append(errs, err)
+	}
+
+	// check hostname
+	hostname, hostnameExists := svc.Cfg.Config["hostname"].(string)
+	if !hostnameExists {
+		err := fmt.Errorf("graphite: hostname not set in config")
+		logger.Error(err.Error())
+		errs = append(errs, err)
+	}
+
+	if len(errs) > 0 {
+		return errs
+	}
+
+	// set host and port
+	svc.Host = hostname
+	svc.Port = port
+
+	logger.Info("Graphite configured to connect to", "host", svc.Host, "port", svc.Port)
+
+	return errs
+}
+
+func (svc *Service) Initialize() error {
+
+	messenger := svc.Deps.MustGetMessenger()
+	return messenger.Subscribe(svc.Cfg.Name, svc.Cfg.Subs["graphite"].Name, svc.messageHandler)
+}
+
+func (svc *Service) messageHandler(msg core.Message) error {
+	logger := svc.Deps.MustGetLogger()
+	logger.Debug("graphite message", "msg", msg)
+
+	value := fmt.Sprintf("%2.2f", msg.Metric)
+
+	unixTime := time.Now().Unix()
+	logger.Info("Sending to Graphite:",
+		"time", time.Unix(unixTime, 0).Format("2006-01-02 15:04:05"),
+		"service", msg.ServiceName,
+		"value", value,
+	)
+	metric := graphite.NewMetric(msg.ServiceName, fmt.Sprintf("%v", value), unixTime)
+
+	if svc.Graphite == nil {
+		logger.Warn("Graphite connection is nil, attempting to connect to Graphite", "host", svc.Host, "port", svc.Port)
+		var err error
+		svc.Graphite, err = graphite.NewGraphite(svc.Host, svc.Port)
+		if err != nil {
+			logger.Error("ERROR: failed to connect to Graphite", "err", err.Error())
+			return err
+		}
+	}
+
+	err := svc.Graphite.SendMetric(metric)
+	if err != nil {
+		logger.Error("ERROR: failed sending metric to Graphite", "err", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (svc *Service) Check() error {
+	return nil
+}
