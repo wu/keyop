01155059f90734622aaed6fbea7ebadd7c3d13cd
Alex White <VVu@geekfarm.org>
2026-02-04 16:40:18 -0800

    ci: update build script to include 32-bit raspi

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ env GOOS=linux GOARCH=amd64 go build -o output/keyop-linux-amd64
 echo "Building keyop for linux arm64"
 env GOOS=linux GOARCH=arm64 go build -o output/keyop-linux-arm64
 
+echo "Building keyop for linux arm"
+env GOARM=6 GOOS=linux GOARCH=arm go build -o output/keyop-linux-arm
+
