15eda78719da00a11efc5e5a327aae12873533b6
Alex White <VVu@geekfarm.org>
2026-02-10 21:50:36 -0800

    feat: log to file
    Log to file is now the default behavior.  Colorized logging to stdout can be enabled with command line option.

--- util/dependencies.go
+++ util/dependencies.go
@@ -6,15 +6,23 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/MatusOllah/slogcolor"
 )
 
-func InitializeDependencies() core.Dependencies {
+func InitializeDependencies(console bool) core.Dependencies {
 
 	deps := core.Dependencies{}
 
-	//logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	deps.SetOsProvider(core.OsProvider{})
+
+	var homeErr error
+	home, homeErr := deps.MustGetOsProvider().UserHomeDir()
+	if homeErr != nil {
+		home = "."
+	}
+
 	slogOptions := slogcolor.DefaultOptions
 	slogOptions.SrcFileMode = slogcolor.Nop
 
@@ -24,20 +32,40 @@ func InitializeDependencies() core.Dependencies {
 		slogOptions.Level = slog.LevelDebug
 	}
 
-	logger := slog.New(slogcolor.NewHandler(os.Stderr, slogOptions))
+	var logger *slog.Logger
+	if console {
+		logger = slog.New(slogcolor.NewHandler(os.Stdout, slogOptions))
+	} else {
+		logDir := filepath.Join(home, ".keyop", "logs")
+		if err := os.MkdirAll(logDir, 0755); err != nil {
+			// Fallback to stderr if we can't create the log directory
+			logger = slog.New(slogcolor.NewHandler(os.Stderr, slogOptions))
+			logger.Error("Failed to create log directory", "path", logDir, "error", err)
+		} else {
+			logFileName := "keyop." + time.Now().Format("20060102") + ".log"
+			logFilePath := filepath.Join(logDir, logFileName)
+			f, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				// Fallback to stderr if we can't open the log file
+				logger = slog.New(slogcolor.NewHandler(os.Stderr, slogOptions))
+				logger.Error("Failed to open log file", "path", logFilePath, "error", err)
+			} else {
+				logger = slog.New(slog.NewTextHandler(f, &slog.HandlerOptions{
+					Level: slogOptions.Level,
+				}))
+			}
+		}
+	}
 	deps.SetLogger(logger)
+
+	if homeErr != nil {
+		logger.Warn("Failed to get user home directory, using current directory as fallback", "error", homeErr)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	deps.SetContext(ctx)
 	deps.SetCancel(cancel)
 
-	deps.SetOsProvider(core.OsProvider{})
-
-	home, err := deps.MustGetOsProvider().UserHomeDir()
-	if err != nil {
-		logger.Error("Failed to get user home directory, using current directory as fallback", "error", err)
-		home = "."
-	}
 	dataDir := filepath.Join(home, ".keyop", "data")
 	deps.SetStateStore(core.NewFileStateStore(dataDir, deps.MustGetOsProvider()))
 
