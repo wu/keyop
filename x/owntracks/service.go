bb1fe107caf66b57e6fcddaa171d5564ec91475c
Alex White <VVu@geekfarm.org>
2026-02-12 14:00:07 -0800

    feat: improvements to owntracks
    - use uuid from owntracks message as correlation id
    - send gps location info to gps channel for other services to consume
    - send battery data to metrics channel
    - send region enter/exit events to events channel

--- x/owntracks/service.go
+++ x/owntracks/service.go
@@ -7,12 +7,16 @@ import (
 	"keyop/core"
 	"keyop/util"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type Service struct {
 	Deps core.Dependencies
 	Cfg  core.ServiceConfig
 	Port int
+
+	currentRegions []string
 }
 
 func NewService(deps core.Dependencies, cfg core.ServiceConfig) core.Service {
@@ -29,11 +33,11 @@ func NewService(deps core.Dependencies, cfg core.ServiceConfig) core.Service {
 	return svc
 }
 
-func (svc Service) ValidateConfig() []error {
+func (svc *Service) ValidateConfig() []error {
 	logger := svc.Deps.MustGetLogger()
 	var errs []error
 
-	pubErrs := util.ValidateConfig("pubs", svc.Cfg.Pubs, []string{"owntracks"}, logger)
+	pubErrs := util.ValidateConfig("pubs", svc.Cfg.Pubs, []string{"owntracks", "gps", "metrics", "events"}, logger)
 	if len(pubErrs) > 0 {
 		errs = append(errs, pubErrs...)
 	}
@@ -49,7 +53,7 @@ func (svc Service) ValidateConfig() []error {
 	return errs
 }
 
-func (svc Service) Initialize() error {
+func (svc *Service) Initialize() error {
 	logger := svc.Deps.MustGetLogger()
 
 	mux := http.NewServeMux()
@@ -67,7 +71,7 @@ func (svc Service) Initialize() error {
 	return nil
 }
 
-func (svc Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (svc *Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	logger := svc.Deps.MustGetLogger()
 	messenger := svc.Deps.MustGetMessenger()
 
@@ -92,11 +96,20 @@ func (svc Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	logger.Error("received owntracks message", "data", data)
+	logger.Debug("received owntracks message", "data", data)
 
 	msg := core.Message{
-		ChannelName: "owntracks",
+		ChannelName: svc.Cfg.Pubs["owntracks"].Name,
+		ServiceType: svc.Cfg.Type,
+		ServiceName: svc.Cfg.Name,
 		Data:        data,
+	}
+
+	// use provided uuid as correlation id or generate a new one
+	if uuidVal, ok := data["uuid"].(string); ok && uuidVal != "" {
+		msg.Uuid = uuidVal
+	} else {
+		msg.Uuid = uuid.New().String()
 	}
 
 	err = messenger.Send(msg)
@@ -106,9 +119,119 @@ func (svc Service) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// gps
+	if msgType, ok := data["_type"].(string); ok && msgType == "location" {
+		gpsData := make(map[string]interface{})
+		for _, field := range []string{"lat", "lon", "alt"} {
+			if val, ok := data[field]; ok {
+				gpsData[field] = val
+			}
+		}
+
+		gpsMsg := core.Message{
+			Uuid:        msg.Uuid,
+			ChannelName: svc.Cfg.Pubs["gps"].Name,
+			ServiceType: svc.Cfg.Type,
+			ServiceName: svc.Cfg.Name,
+			Data:        gpsData,
+		}
+		if err := messenger.Send(gpsMsg); err != nil {
+			logger.Error("failed to send gps message", "error", err)
+		}
+	}
+
+	// metrics
+	if batt, ok := data["batt"].(float64); ok {
+		metricsData := make(map[string]interface{})
+		if tid, ok := data["tid"].(string); ok {
+			metricsData["tid"] = tid
+		}
+		if device, ok := data["device"].(string); ok {
+			metricsData["device"] = device
+		}
+
+		metricsMsg := core.Message{
+			Uuid:        msg.Uuid,
+			ChannelName: svc.Cfg.Pubs["metrics"].Name,
+			ServiceType: svc.Cfg.Type,
+			ServiceName: svc.Cfg.Name,
+			Metric:      batt,
+			Data:        metricsData,
+		}
+		if err := messenger.Send(metricsMsg); err != nil {
+			logger.Error("failed to send metrics message", "error", err)
+		}
+	}
+
+	// events
+	if inregions, ok := data["inregions"].([]interface{}); ok {
+		var newRegions []string
+		for _, r := range inregions {
+			if s, ok := r.(string); ok {
+				newRegions = append(newRegions, s)
+			}
+		}
+
+		eventsChannel := svc.Cfg.Pubs["events"].Name
+
+		// check for entered regions
+		for _, nr := range newRegions {
+			found := false
+			for _, cr := range svc.currentRegions {
+				if nr == cr {
+					found = true
+					break
+				}
+			}
+			if !found {
+				eventMsg := core.Message{
+					Uuid:        msg.Uuid,
+					ChannelName: eventsChannel,
+					ServiceType: svc.Cfg.Type,
+					ServiceName: svc.Cfg.Name,
+					Data: map[string]interface{}{
+						"event":  "enter",
+						"region": nr,
+					},
+				}
+				if err := messenger.Send(eventMsg); err != nil {
+					logger.Error("failed to send enter event message", "error", err)
+				}
+			}
+		}
+
+		// check for exited regions
+		for _, cr := range svc.currentRegions {
+			found := false
+			for _, nr := range newRegions {
+				if cr == nr {
+					found = true
+					break
+				}
+			}
+			if !found {
+				eventMsg := core.Message{
+					Uuid:        msg.Uuid,
+					ChannelName: eventsChannel,
+					ServiceType: svc.Cfg.Type,
+					ServiceName: svc.Cfg.Name,
+					Data: map[string]interface{}{
+						"event":  "exit",
+						"region": cr,
+					},
+				}
+				if err := messenger.Send(eventMsg); err != nil {
+					logger.Error("failed to send exit event message", "error", err)
+				}
+			}
+		}
+
+		svc.currentRegions = newRegions
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
 
-func (svc Service) Check() error {
+func (svc *Service) Check() error {
 	return nil
 }
