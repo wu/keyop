29ef2e86e28fe9913e5e89372a3a01282a9bc935
Alex White <VVu@geekfarm.org>
2026-02-17 16:37:58 -0800

    feat: config skips service types in anomaly detector

--- x/anomalyDetector/service.go
+++ x/anomalyDetector/service.go
@@ -81,7 +81,7 @@ func (svc *Service) Initialize() error {
 func (svc *Service) messageHandler(msg core.Message) error {
 
 	for _, skipService := range svc.SkipServices {
-		if msg.ServiceName == skipService {
+		if msg.ServiceType == skipService {
 			return nil
 		}
 	}
