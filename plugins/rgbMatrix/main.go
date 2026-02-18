3c6af9abe8bef7e56c4086070b85b569e193b988
Alex White <VVu@geekfarm.org>
2026-02-18 00:58:23 -0800

    feat: improvements to rgb matrix
    - handle cancel
    - option to swap green and blue
    - update display color

--- plugins/rgbMatrix/main.go
+++ plugins/rgbMatrix/main.go
@@ -74,6 +74,11 @@ func (p *RGBMatrixPlugin) Initialize() error {
 		return err
 	}
 
+	err = p.canvas.Clear()
+	if err != nil {
+		return fmt.Errorf("failed to clear canvas: %w", err)
+	}
+
 	p.deps.MustGetLogger().Info("RGBMatrixPlugin initialized", "rows", config.Rows, "cols", config.Cols)
 	return nil
 }
@@ -107,7 +112,33 @@ func (p *RGBMatrixPlugin) colorRGBA(r, g, b, a uint8) color.RGBA {
 
 func (p *RGBMatrixPlugin) Check() error {
 	logger := p.deps.MustGetLogger()
-	logger.Warn("RGBMatrixPlugin Check called - this plugin runs continuously and does not support health checks")
+	logger.Warn("RGBMatrixPlugin Check called")
+
+	ctx := p.deps.MustGetContext()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	// Initial render
+	if err := p.Render(); err != nil {
+		logger.Error("Failed to render", "error", err)
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Info("RGBMatrixPlugin Check context cancelled")
+			return ctx.Err()
+		case <-ticker.C:
+			if err := p.Render(); err != nil {
+				logger.Error("Failed to render", "error", err)
+			}
+		}
+	}
+}
+
+func (p *RGBMatrixPlugin) Render() error {
+	logger := p.deps.MustGetLogger()
+	logger.Warn("RGBMatrixPlugin Render called")
 
 	if p.canvas == nil {
 		return fmt.Errorf("canvas not initialized")
@@ -115,49 +146,43 @@ func (p *RGBMatrixPlugin) Check() error {
 
 	logger.Warn("Foo: RGBMatrixPlugin Check passed")
 
-	p.canvas.Clear()
+	t := time.Now()
+	timeStr := t.Format("3:04pm")
+	dayOfWeek := t.Format("Mon")
+	dayOfMonth := t.Format("_2") // Day of month (1-31)
 
-	for {
-		t := time.Now()
-		timeStr := t.Format("3:04pm")
-		dayOfWeek := t.Format("Mon")
-		dayOfMonth := t.Format("_2") // Day of month (1-31)
+	p.deps.MustGetLogger().Debug("RGBMatrixPlugin displaying info", "time", timeStr, "day", dayOfWeek, "date", dayOfMonth)
 
-		p.deps.MustGetLogger().Debug("RGBMatrixPlugin displaying info", "time", timeStr, "day", dayOfWeek, "date", dayOfMonth)
+	// Create an image to draw the text onto
+	bounds := p.canvas.Bounds()
+	img := image.NewRGBA(bounds)
 
-		// Create an image to draw the text onto
-		bounds := p.canvas.Bounds()
-		img := image.NewRGBA(bounds)
-
-		// Draw text to the image
-		d := &font.Drawer{
-			Dst: img,
-			Src: image.NewUniform(p.colorRGBA(100, 0, 255, 255)),
-		}
-
-		// 1. Draw Day of Week (Upper Left)
-		d.Face = p.dayOfWeekFace
-		d.Dot = fixed.Point26_6{X: fixed.I(0), Y: fixed.I(6)}
-		d.DrawString(dayOfWeek)
-
-		// 2. Draw Day of Month (Below Day of Week)
-		d.Face = p.dayOfMonthFace
-		d.Dot = fixed.Point26_6{X: fixed.I(0), Y: fixed.I(11)}
-		d.DrawString(dayOfMonth)
-
-		// 3. Draw Time (Centered)
-		d.Face = p.timeFace
-		d.Dot = fixed.Point26_6{X: fixed.I(20), Y: fixed.I(10)}
-		d.DrawString(timeStr)
-
-		// Copy image pixels to canvas
-		draw.Draw(p.canvas, bounds, img, image.Point{}, draw.Over)
-
-		p.canvas.Render()
-		logger.Warn("RGBMatrixPlugin Render called")
-
-		time.Sleep(1 * time.Second)
+	// Draw text to the image
+	d := &font.Drawer{
+		Dst: img,
+		Src: image.NewUniform(p.colorRGBA(100, 0, 255, 255)),
 	}
+
+	// 1. Draw Day of Week (Upper Left)
+	d.Face = p.dayOfWeekFace
+	d.Dot = fixed.Point26_6{X: fixed.I(0), Y: fixed.I(6)}
+	d.DrawString(dayOfWeek)
+
+	// 2. Draw Day of Month (Upper Left, Below Day of Week)
+	d.Face = p.dayOfMonthFace
+	d.Dot = fixed.Point26_6{X: fixed.I(0), Y: fixed.I(11)}
+	d.DrawString(dayOfMonth)
+
+	// 3. Draw Time (Top Center/Right)
+	d.Face = p.timeFace
+	d.Dot = fixed.Point26_6{X: fixed.I(20), Y: fixed.I(10)}
+	d.DrawString(timeStr)
+
+	// Copy image pixels to canvas
+	draw.Draw(p.canvas, bounds, img, image.Point{}, draw.Over)
+
+	p.canvas.Render()
+	logger.Warn("RGBMatrixPlugin Render called")
 
 	return nil
 }
