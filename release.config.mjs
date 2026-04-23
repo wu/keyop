8e51ea49fc9472028d55220b542b21c0fe110e96
Alex White <VVu@geekfarm.org>
2026-04-22 23:15:30 -0700

    ci: update build

---
+++ release.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
